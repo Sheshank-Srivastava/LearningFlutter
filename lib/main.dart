435fe8119463deda2db61f34d925d8bba9be55d1
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-19 13:58:26 +0530

    TabBar View

--- lib/main.dart
+++ lib/main.dart
@@ -6,72 +6,71 @@ void main() {
   ));
 }
 
-class MyItem {
-  bool isExpanded;
-  final String header;
-  final Widget body;
-
-  MyItem(this.isExpanded, this.header, this.body);
-}
-
 class MyApp extends StatefulWidget {
   @override
   _State createState() => new _State();
 }
 
-class _State extends State<MyApp> {
-  List<MyItem> _items = new List<MyItem>();
+class Choice {
+  final String title;
+  final IconData icon;
+
+  const Choice({this.title, this.icon});
+}
+
+class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController _controller;
+  List<Choice> _items = const <Choice>[
+    const Choice(title: 'CAR', icon: Icons.directions_car),
+    const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
+    const Choice(title: 'BOAT', icon: Icons.directions_boat),
+    const Choice(title: 'TRAIN', icon: Icons.directions_railway),
+    const Choice(title: 'WALK', icon: Icons.directions_walk),
+  ];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    for (int i = 0; i < 10; i++) {
-      _items.add(
-          new MyItem(
-              false,
-              'Item ${i}',
-              new Container(
-                padding: new EdgeInsets.all(10.0),
-                child: new Text('Hello World'),
-              )));
-    }
-  }
-
-  ExpansionPanel _createItems(MyItem item) {
-    return new ExpansionPanel(
-        headerBuilder: (BuildContext context, bool isExpanded) {
-          return new Container(
-            padding: new EdgeInsets.all(5.0),
-            child: new Text('Header ${item.header}'),
-          );
-        },
-        body: item.body,
-        isExpanded: item.isExpanded);
+    _controller = new TabController(length: _items.length, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name Here'),
-        backgroundColor: Colors.pink,
-      ),
-      body: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new ListView(
-            children: [
-              new ExpansionPanelList(
-                expansionCallback: (int index, bool isExpanded) {
-                  setState(() {
-                    _items[index].isExpanded = !_items[index].isExpanded;
-                  });
-                },
-                children: _items.map(_createItems).toList(),
-              )
-            ],
-          )
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text('Name Here'),
+          bottom: new PreferredSize(
+            preferredSize: const Size.fromHeight(48.0),
+            child: new Theme(
+                data: Theme.of(context).copyWith(accentColor: Colors.white),
+                child: new Container(
+                  padding: new EdgeInsets.all(1.0),
+                  height: 48.0,
+                  alignment: Alignment.center,
+                  child: new TabPageSelector(
+                    controller: _controller,
+                  ),
+                )),
+          ),
+        ),
+        body: new TabBarView(
+            controller: _controller,
+            children: _items.map((Choice item) {
+              return new Container(
+                padding: new EdgeInsets.all(25.0),
+                child: new Center(
+                  child: new Column(
+                    children: [
+                      new Text(item.title),
+                      new Icon(
+                        item.icon,
+                        size: 120.0,
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }).toList()));
   }
 }
