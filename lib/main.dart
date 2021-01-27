d3888fcc3f6f8031d0355ee575a18757e105fd41
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-27 13:13:27 +0530

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'mywidget.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -13,60 +12,22 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class Sales {
-  int year;
-  int sales;
-  charts.Color color;
-
-  Sales(this.year, this.sales, this.color);
-}
-
 class _State extends State<MyApp> {
-  List<Sales> _data;
-  List<charts.Series<Sales, int>> _chartdata;
-
-  void _makeData() {
-    _chartdata = new List<charts.Series<Sales, int>>();
-    _data = <Sales>[
-      new Sales(0, 100, charts.MaterialPalette.red.shadeDefault),
-      new Sales(1, 75, charts.MaterialPalette.blue.shadeDefault),
-      new Sales(2, 25, charts.MaterialPalette.green.shadeDefault),
-      new Sales(3, 5, charts.MaterialPalette.yellow.shadeDefault)
-    ];
-    _chartdata.add(new charts.Series(
-        id: 'Sales',
-        data: _data,
-        colorFn: (Sales sales, _) => sales.color,
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales));
-  }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _makeData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name Here'),
         backgroundColor: Colors.pink,
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32.0),
+        padding:  new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Sales Data'),
-              new Expanded(
-                  child: new charts.PieChart(
-                _chartdata,
-                animate: true,
-                animationDuration: new Duration(seconds: 5),
-              ))
+              new Text('Hello World'),
+              new MyWidget()
             ],
           ),
         ),
---
+++ lib/mywidget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      padding: new EdgeInsets.all(32.0),
+      child: new Text('Hello World'),
+    );
+  }
+}
