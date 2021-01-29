c7526d485ce9d100d3f014adf2af16e31ab8cdeb
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-29 17:04:34 +0530

    Inteactive marker

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  var markers = <Marker>[
-    new Marker(
-        width: 80.0,
-        height: 80.0,
-        point: new LatLng(41.8781, -87.6298),
-        builder: (ctx)=> new Icon(Icons.pin_drop,color: Colors.red,)
-    ),new Marker(
-        width: 80.0,
-        height: 80.0,
-        point: new LatLng(42.8781, -87.6298),
-        builder: (ctx)=> new Icon(Icons.pin_drop,color: Colors.red,)
-    ),new Marker(
-        width: 80.0,
-        height: 80.0,
-        point: new LatLng(43.8781, -87.6298),
-        builder: (ctx)=> new Icon(Icons.pin_drop,color: Colors.red,)
-    )
-  ];
+  MapController mapController;
+  Map<String, LatLng> coords;
+  List<Marker> markers;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mapController = new MapController();
+    coords = new Map<String, LatLng>();
+    coords.putIfAbsent('Chicago', () => new LatLng(41.8781, -87.6298));
+    coords.putIfAbsent('Detroit', () => new LatLng(42.3314, -83.0458));
+    coords.putIfAbsent('Lansing', () => new LatLng(42.7325, -84.5555));
+    markers = new List<Marker>();
+    for (int i = 0; i < coords.length; i++) {
+      markers.add(new Marker(
+          width: 80.0,
+          height: 80.0,
+          point: coords.values.elementAt(i),
+          builder: (ctx) =>
+          new Icon(
+            Icons.pin_drop,
+            color: Colors.red,
+          )));
+    }
+  }
+
+  void _showCord(int index) {
+    mapController.move(coords.values.elementAt(index), 10.0);
+  }
+
+  List<Widget> _makeButtons() {
+    List<Widget> list = new List<Widget>();
+    for(int i=0;i<coords.length;i++) {
+      list.add(new RaisedButton(
+          onPressed: () => _showCord(i),
+          child: new Text(coords.keys.elementAt(i))
+      ));
+    }
+    return list;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,21 +68,22 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
+              new Row(
+                children: _makeButtons(),
+              ),
               new Flexible(
                 child: new FlutterMap(
+                  mapController: mapController,
                   options: new MapOptions(
-                      center: new LatLng(41.8781, -87.6298),
-                      zoom: 12.0
-                  ),
+                      center: new LatLng(41.8781, -87.6298), zoom: 12.0),
                   layers: [
                     new TileLayerOptions(
-                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                        subdomains: ['a', 'b', 'c']
-                    ),
-                    new MarkerLayerOptions(
-                      markers: markers
-                    )
+                        urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c']),
+                      new MarkerLayerOptions(markers: markers),
                   ],
+
                 ),
               )
             ],
