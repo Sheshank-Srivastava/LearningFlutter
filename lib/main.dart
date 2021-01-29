f43434fcfe6caa9fb10e98264aaae6b10141d868
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-29 15:14:35 +0530

    Add Open Street map to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -12,58 +13,7 @@ class MyApp extends StatefulWidget {
   _State createState() => new _State();
 }
 
-class AnimatedLogo extends AnimatedWidget{
-
-  static final _opacityTween = new Tween<double>(begin: 0.0,end: 1.0);
-  static final _sizeTween = new Tween<double>(begin: 0.0,end: 300.0);
-  static final _rotateTween = new Tween<double>(begin: 0.0,end: 12.0);
-  AnimatedLogo({Key key, Animation<double> animation}): super(key: key,listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    final Animation<double> animation = listenable;
-
-    return new Center(
-      child: new Transform.rotate(
-        angle: _rotateTween.evaluate(animation),
-        child: new Opacity(
-          opacity: _opacityTween.evaluate(animation),
-          child: new Container(
-            margin: new EdgeInsets.symmetric(vertical: 10.0),
-            height: _sizeTween.evaluate(animation),
-            width: _sizeTween.evaluate(animation),
-            child: new FlutterLogo(),
-          )
-        )
-      ),
-    );
-
-  }
-
-}
-
-class _State extends State<MyApp> with TickerProviderStateMixin{
-  Animation animation;
-  AnimationController controller;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller = new AnimationController(duration: const Duration(milliseconds: 2000),vsync: this);
-    animation = new CurvedAnimation(parent: controller,curve: Curves.easeIn);
-    animation.addStatusListener(listener);
-    controller.forward();
-  }
-
-  void listener(AnimationStatus status){
-    if(status == AnimationStatus.completed){
-      controller.reverse();
-    }else if(status == AnimationStatus.dismissed){
-      controller.forward();
-    }
-  }
+class _State extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +24,26 @@ class _State extends State<MyApp> with TickerProviderStateMixin{
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
-        child: new AnimatedLogo(animation: animation,),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Flexible(
+                  child: new FlutterMap(
+                      options: new MapOptions(
+                        center: new LatLng(41.8781,-87.6298),
+                        zoom: 12.0
+                      ),
+                    layers: [
+                      new TileLayerOptions(
+                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a','b','c']
+                      )
+                    ],
+                  ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
